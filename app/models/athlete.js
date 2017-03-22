991a2e05d7ba693b05a3cc10963c8e548d4c289b
João Paulo Canário <jopacanario@gmail.com>
2017-03-22 17:34:22 -0300

    Code refactor

--- app/models/athlete.js
+++ app/models/athlete.js
@@ -3,36 +3,12 @@ var findOrCreate = require('mongoose-findorcreate');
 var Schema = mongoose.Schema;
 
 var AthleteSchema = new Schema({
-  id: String,
-  name: String,
-  affiliate: String,
-  sex: String,
-  overallScore: Number,
-  wod1Display: String,
-  wod1Score: Number,
-  wod1Rank: Number,
-  wod1TieBreak: Number,
-  wod1IsScale: Boolean,
-  wod2Display: String,
-  wod2Score: Number,
-  wod2Rank: Number,
-  wod2TieBreak: Number,
-  wod2IsScale: Boolean,
-  wod3Display: String,
-  wod3Score: Number,
-  wod3Rank: Number,
-  wod3TieBreak: Number,
-  wod3IsScale: Boolean,
-  wod4Display: String,
-  wod4Score: Number,
-  wod4Rank: Number,
-  wod4TieBreak: Number,
-  wod4IsScale: Boolean,
-  wod5Display: String,
-  wod5Score: Number,
-  wod5Rank: Number,
-  wod5TieBreak: Number,
-  wod5IsScale: Boolean
+  id: String, name: String, affiliate: String, sex: String, overallScore: Number,
+  wod1Display: String, wod1Score: Number, wod1Rank: Number, wod1TieBreak: Number, wod1IsScale: Boolean,
+  wod2Display: String, wod2Score: Number, wod2Rank: Number, wod2TieBreak: Number, wod2IsScale: Boolean,
+  wod3Display: String, wod3Score: Number, wod3Rank: Number, wod3TieBreak: Number, wod3IsScale: Boolean,
+  wod4Display: String, wod4Score: Number, wod4Rank: Number, wod4TieBreak: Number, wod4IsScale: Boolean,
+  wod5Display: String, wod5Score: Number, wod5Rank: Number, wod5TieBreak: Number, wod5IsScale: Boolean
 });
 
 AthleteSchema.plugin(findOrCreate);
