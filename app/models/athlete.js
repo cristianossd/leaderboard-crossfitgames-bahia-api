050d8bd512637ab15a81d8ca4a2045505523baf4
João Paulo Canário <jopacanario@gmail.com>
2017-03-22 17:34:04 -0300

    Added scales to leaderboard
    Fixed major issue: athletes/sync saving duplicated athletes.
    
    Fixed minor issue: Some sex codes are not included
    
    Fixed minor issue: set undefined overall score
    
    Refactor of ranking generator
    
    Created new .json data with scales athletes
    
    Fixed minor issues

--- app/models/athlete.js
+++ app/models/athlete.js
@@ -12,22 +12,27 @@ var AthleteSchema = new Schema({
   wod1Score: Number,
   wod1Rank: Number,
   wod1TieBreak: Number,
+  wod1IsScale: Boolean,
   wod2Display: String,
   wod2Score: Number,
   wod2Rank: Number,
   wod2TieBreak: Number,
+  wod2IsScale: Boolean,
   wod3Display: String,
   wod3Score: Number,
   wod3Rank: Number,
   wod3TieBreak: Number,
+  wod3IsScale: Boolean,
   wod4Display: String,
   wod4Score: Number,
   wod4Rank: Number,
   wod4TieBreak: Number,
+  wod4IsScale: Boolean,
   wod5Display: String,
   wod5Score: Number,
   wod5Rank: Number,
-  wod5TieBreak: Number
+  wod5TieBreak: Number,
+  wod5IsScale: Boolean
 });
 
 AthleteSchema.plugin(findOrCreate);
